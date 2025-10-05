fbee6b4ec13bcd2654e699267d93ea860e463880
hy040911 <2909526119@qq.com>
2025-10-05 15:45:31 +0800

    八、展示页面可视化修改
    使用 vite 工具配置代理实现同源
    配置父页面对子页面的脚本注入、利用 postmessage 实现子页面对父页面的通信

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // 配置代理，实现跨域（浏览器同源策略）
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8123',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
